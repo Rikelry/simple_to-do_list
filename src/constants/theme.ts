5991444e9bd0d9b22d19a3c485e4bda1e65f510e
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-05-14 12:01:34 -0300

    feat: theme.ts

---
+++ src/constants/theme.ts
@@ -0,0 +1,11 @@
+export type Theme = {
+  bg: string;
+  card: string;
+  cardSoft: string;
+  text: string;
+  subText: string;
+  border: string;
+  accent: string;
+  accentText: string;
+  inputBg: string;
+};
