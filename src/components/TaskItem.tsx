0ed1b9c65a64507a395576e98145de10fe402e26
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-05-14 22:18:16 -0300

    feat: type TaskItemProps in TaskItem.tsx

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,2 +1,10 @@
 import type { Theme } from '../constants/theme';
 import type { Task } from '../types/task';
+
+type TaskItemProps = {
+  theme: Theme;
+  task: Task;
+  onToggle: (id: string) => void;
+  onEdit: (task: Task) => void;
+  onDelete: (id: string) => void;
+};
