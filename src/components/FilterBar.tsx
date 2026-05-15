3ba334e1c792824e00bdc5b403ea5e9c04e1ca0a
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-05-14 22:11:10 -0300

    feat: type FilterBarProps in FilterBar.tsx

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,2 +1,9 @@
 import { FILTERS, type Theme } from '../constants/theme';
 import type { Filter } from '../types/task';
+
+type FilterBarProps = {
+    theme: Theme;
+    filter: Filter;
+    tasksLeft: number;
+    onChangeFilter: (value: Filter) => void;
+};
