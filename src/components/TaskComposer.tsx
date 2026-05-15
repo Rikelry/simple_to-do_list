9922960baf2b2c184acaa66000ff1137719d1c21
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-05-14 22:02:14 -0300

    type: TaskComposerProps in TaskComposer.tsx

--- src/components/TaskComposer.tsx
+++ src/components/TaskComposer.tsx
@@ -1,1 +1,8 @@
 import type { Theme } from '../constants/theme';
+
+type TaskComposerProps = {
+    theme: Theme;
+    value: string;
+    onChangeText: (text: string) => void;
+    onSubmit: () => void;
+};
