359fd92092eb392c6bc1147a08b3a93787944c99
Rikelry <153693133+Rikelry@users.noreply.github.com>
2026-05-14 21:46:31 -0300

    feat: tipos de Sidebar Props

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,1 +1,8 @@
 import type { Theme } from '../constants/theme';
+
+type SidebarProps = {
+    theme: Theme;
+    isDark: boolean;
+    onToggleTheme: () => void;
+    onClearCompleted: () => void;
+};
